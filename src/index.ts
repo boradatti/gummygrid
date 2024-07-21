aaa91c4c283efc2c44c1afda8c3253773a72eb00
Boris Adaev <mr.bobod@mail.ru>
2024-07-21 10:19:04 +0300

    exported svg type

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 export { default } from '@/generator';
 
 export type { GummyGridConfig } from '@/generator/types';
+export type { default as SVG } from '@/svg';
 export type { SVGColor } from '@/svg/types';
